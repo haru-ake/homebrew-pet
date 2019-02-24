1cc39c15b72fb125c9b23532bac99a8e2e061d8b
HARU Akebono <haru.akebono.11@gmail.com>
2019-02-24 16:58:23 +0900

    support Linuxbrew

--- pet.rb
+++ pet.rb
@@ -1,10 +1,16 @@
 class Pet < Formula
   desc "Simple command-line snippet manager"
   homepage "https://github.com/knqyf263/pet"
-  url "https://github.com/knqyf263/pet/releases/download/v0.3.4/pet_0.3.4_darwin_amd64.tar.gz"
   version "0.3.4"
-  sha256 "cf542863763d0500b5f2247697f2c1025add03b6beaff643896da15749163de4"
-  
+
+  if OS.mac?
+    url "https://github.com/knqyf263/pet/releases/download/v0.3.4/pet_0.3.4_darwin_amd64.tar.gz"
+    sha256 "cf542863763d0500b5f2247697f2c1025add03b6beaff643896da15749163de4"
+  else
+    url "https://github.com/knqyf263/pet/releases/download/v0.3.4/pet_0.3.4_linux_amd64.tar.gz"
+    sha256 "5f5012b5c90236d3f908a770c2fe6de59ed1c92ac88c35cb99a950a850b8563d"
+  end
+
   depends_on "fzf"
 
   def install
